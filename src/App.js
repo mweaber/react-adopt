e6c135662d08a478705c95ef78896f728b1ff6ad
Matthew Weaber <matthewweaber89@gmail.com>
2020-07-20 13:41:16 -0400

    hitting api, use state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement("div", { id: "generic-id-name" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Scarlet",
-      animal: "dog",
-      breed: "Shiba Inu",
-    }),
-    React.createElement(Pet, {
-      name: "Boris",
-      animal: "Reptile",
-      breed: "Tortoise",
-    }),
-    React.createElement(Pet, {
-      name: "Boba",
-      animal: "Reptile",
-      breed: "Bearded Dragon",
-    }),
-  ]);
+  // return React.createElement("div", { id: "generic-id-name" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Scarlet",
+  //     animal: "Dog",
+  //     breed: "Shiba Inu",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Boris",
+  //     animal: "Reptile",
+  //     breed: "Tortoise",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Boba",
+  //     animal: "Reptile",
+  //     breed: "Bearded Dragon",
+  //   }),
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));

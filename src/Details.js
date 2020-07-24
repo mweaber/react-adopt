d6ecebff6b9f3f7334a919ac6e501ebca4a4e84d
Matthew Weaber <matthewweaber89@gmail.com>
2020-07-24 13:53:52 -0400

    added error handling

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
-import Carousel from './Carousel'
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class Details extends React.Component {
   }
 
   componentDidMount() {
+    // throw new Error ('Lol');
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -47,4 +49,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
